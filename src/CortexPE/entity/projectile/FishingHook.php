5bc6af1ac97cca4731fd3b9e8c16bbe83c5af904
CortexPE <CortexPE@users.noreply.github.com>
2017-12-22 05:34:58 -0600

    Fix #94 and update License Header

--- src/CortexPE/entity/projectile/FishingHook.php
+++ src/CortexPE/entity/projectile/FishingHook.php
@@ -1,23 +1,35 @@
 <?php
 
-/*
+/**
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ * MMP""MM""YMM               .M"""bgd
+ * P'   MM   `7              ,MI    "Y
+ *      MM  .gP"Ya   ,6"Yb.  `MMb.   `7MMpdMAo.  ,pW"Wq.   ,pW"Wq.`7MMpMMMb.
+ *      MM ,M'   Yb 8)   MM    `YMMNq. MM   `Wb 6W'   `Wb 6W'   `Wb MM    MM
+ *      MM 8M""""""  ,pm9MM  .     `MM MM    M8 8M     M8 8M     M8 MM    MM
+ *      MM YM.    , 8M   MM  Mb     dM MM   ,AP YA.   ,A9 YA.   ,A9 MM    MM
+ *    .JMML.`Mbmmd' `Moo9^Yo.P"Ybmmd"  MMbmmd'   `Ybmd9'   `Ybmd9'.JMML  JMML.
+ *                                     MM
+ *                                   .JMML.
+ * This file is part of TeaSpoon.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * TeaSpoon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * TeaSpoon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with TeaSpoon.  If not, see <http://www.gnu.org/licenses/>.
  *
-*/
+ * @author CortexPE
+ * @link https://CortexPE.xyz
+ *
+ */
 
 declare(strict_types = 1);
 
@@ -72,7 +84,7 @@ class FishingHook extends Projectile {
 			$pk = new EntityEventPacket();
 			$pk->entityRuntimeId = $this->getId();
 			$pk->event = EntityEventPacket::FISH_HOOK_POSITION;
-			PMServer::getInstance()->broadcastPacket($this->getLevel()->getPlayers(), $pk);
+			PMServer::getInstance()->broadcastPacket($this->getViewers(), $pk);
 		}
 
 		if($this->attractTimer === 0 && mt_rand(0, 100) <= 30){
