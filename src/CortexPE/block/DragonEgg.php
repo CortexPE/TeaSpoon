613991c5a6df0cab1cacd179b43701d534c5e184
CortexPE <mcpe4life62@gmail.com>
2019-05-13 06:15:08 -0700

    haha yes

--- src/CortexPE/block/DragonEgg.php
+++ src/CortexPE/block/DragonEgg.php
@@ -23,20 +23,13 @@ declare(strict_types = 1);
 namespace CortexPE\block;
 
 use CortexPE\Main;
-use pocketmine\block\{
-	Air, Fallable
-};
+use pocketmine\block\{Air, Block, Fallable};
 use pocketmine\item\Item;
-use pocketmine\level\{
-	Level, Position, sound\GenericSound
-};
+use pocketmine\level\{Position, sound\GenericSound};
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 
 class DragonEgg extends Fallable {
-
-	const RAND_VERTICAL = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
-	const RAND_HORIZONTAL = [-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 	/** @var int $id */
 	protected $id = self::DRAGON_EGG;
@@ -100,30 +93,26 @@ class DragonEgg extends Fallable {
 	 */
 	public function onActivate(Item $item, Player $player = null): bool{
 		if(Main::$dragonEggTeleport){
-			$found = false;
 			$level = $this->getLevel();
-			$x = $y = $z = 0;
 			for($c = 0; $c <= 16; $c++){
-				$x = $this->getX() + self::RAND_HORIZONTAL[array_rand(self::RAND_HORIZONTAL)];
-				$y = $this->getY() + self::RAND_VERTICAL[array_rand(self::RAND_VERTICAL)];
-				$z = $this->getZ() + self::RAND_HORIZONTAL[array_rand(self::RAND_HORIZONTAL)];
-				if($level->getBlockIdAt($x, $y, $z) == 0 && $y < Level::Y_MAX && $y > 0){
-					$found = true;
-					break;
-				}
-			}
+				$x = $this->getX() + mt_rand(-15, 15);
+				$y = $this->getY() + mt_rand(-7, 7);
+				$z = $this->getZ() + mt_rand(-15, 15);
 
-			if($found){
-				$level->setBlock($this, new Air(), true, true);
-				$oldpos = clone $this;
-				$pos = new Vector3($x, $y, $z);
-				$newpos = clone $pos;
-				$level->setBlock($pos, $this, true, true);
-				$posdistance = new Vector3($newpos->x - $oldpos->x, $newpos->y - $oldpos->y, $newpos->z - $oldpos->z);
-				$intdistance = $oldpos->distance($newpos);
-				for($c = 0; $c <= $intdistance; $c++){
-					$progress = $c / $intdistance;
-					$this->getLevel()->addSound(new GenericSound(new Position($oldpos->x + $posdistance->x * $progress, 1.62 + $oldpos->y + $posdistance->y * $progress, $oldpos->z + $posdistance->z * $progress, $this->getLevel()), 2010));
+				if($level->getBlockIdAt($x, $y, $z) == Block::AIR && $level->isInWorld($x, $y, $z)) {
+					$level->setBlock($this, new Air(), true, true);
+					$oldPos = $this->asVector3();
+					$level->setBlock(($pos = new Vector3($x, $y, $z)), $this, true, true);
+					$posDelta = $pos->subtract($oldPos);
+					$dist = $oldPos->distance($pos);
+					for($c = 0; $c <= $dist; $c++) {
+						$progress = $c / $dist;
+						$this->getLevel()->addSound(new GenericSound(new Position($oldPos->x + $posDelta->x * $progress,
+							1.62 + $oldPos->y + $posDelta->y * $progress, $oldPos->z + $posDelta->z * $progress,
+							$this->getLevel()), 2010));
+					}
+
+					return true;
 				}
 			}
 		}
